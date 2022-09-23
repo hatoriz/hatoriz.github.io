d03de016cfa6d5c75d897dab0e6747892cdc9b6d
Nathdanai Kwansiripat <ninja.hatoriz@gmail.com>
2022-09-23 14:58:28 +0700

    chore: remove all existing files

--- More/contents/เครดิตภาษีเงินปันผล (ตอนแรก)_files/sanook_em.js
+++
@@ -1,5 +0,0 @@
-(function() {
-	var em = document.createElement('script'); em.type = 'text/javascript'; em.async = true;
-	em.src = ('https:' == document.location.protocol ? 'https://c' : 'http://c-cdn') + '.effectivemeasure.net/em.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(em, s);
-})();
